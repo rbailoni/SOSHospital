2d7ebd59190e48c4b610a482800238ba30975755
Swift-Noturno <swift-noturno@wozmac2-3.local>
2015-10-27 19:53:56 -0200

    - Inicio do projeto

---
+++ SOSHospital/SOSHospital/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SOSHospital
+//
+//  Created by Swift-Noturno on 27/10/15.
+//  Copyright © 2015 Quaddro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
